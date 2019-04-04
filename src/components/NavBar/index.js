5ead7eefa15f245c15b3284e61a95b7e0c2c738e
Christine Joy Villaruel <cjpvillaruel@gmail.com>
2019-04-04 11:09:40 +0800

    Add prettier and add precommit hook for linter

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   Navbar,
   NavbarToggler,
@@ -17,7 +17,7 @@ import {
   InputGroupText,
   FormInput,
   Collapse
-} from "shards-react";
+} from 'shards-react';
 
 export default class NavExample extends React.Component {
   constructor(props) {
@@ -40,7 +40,6 @@ export default class NavExample extends React.Component {
       }
     });
   }
-
   toggleNavbar() {
     this.setState({
       ...this.state,
